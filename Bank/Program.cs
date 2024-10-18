35ca9982befee53537f74575c5a35052aec954a6
Lizi Shvelidze <l.shvelidze09@gmail.com>
2024-10-18 23:28:24 +0400

    homework 9

---
+++ Bank/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Bank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFinanceOperations bank = new Bank();
+            IFinanceOperations microFinance = new MicroFinance();
+
+            Console.WriteLine("Input months");
+            int months = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input amount per month");
+            double AmountPerMonth = double.Parse(Console.ReadLine());
+
+            bank.CalculateLoanPercent(months, AmountPerMonth);
+            microFinance.CalculateLoanPercent(months, AmountPerMonth);
+        }
+
+        interface IFinanceOperations
+        {
+            void CalculateLoanPercent(int month, double AmountPerMonth);
+            bool CheckUserHistory();
+        }
+        public class Bank : IFinanceOperations
+        {
+            public Random random = new Random();
+
+            public bool CheckUserHistory()
+            {
+                return random.Next(0, 2) == 1; // 1 = true, 0 = false, 
+            }
+            public void CalculateLoanPercent(int month, double AmountPerMonth)
+            {
+                if (CheckUserHistory())
+                {
+                    double totalLoan = AmountPerMonth * month;
+                    double fee = totalLoan * 0.05; // 5% საკომისიო
+                    double amountToPay = totalLoan + fee;
+
+                    Console.WriteLine($"Amount to pay after {month} months is {amountToPay}");
+                }
+                else
+                    Console.WriteLine("Bank user history check failed. Loan is not approved.");
+            }
+        }
+        public class MicroFinance : IFinanceOperations
+        {
+            public bool CheckUserHistory()
+            {
+                return true;
+            }
+            public void CalculateLoanPercent(int month, double AmountPerMonth)
+            {
+                double totalLoan = AmountPerMonth * month;
+                double fee = totalLoan * 0.10; // 10% საკომისიო
+                double serviceFee = 4 * month; // 4 დოლარიანი საკომისიო ყოველთვიურად
+                double amountToPay = totalLoan + fee + serviceFee;
+
+                Console.WriteLine($"Amount to pay after {month} months is {amountToPay}");
+            }
+        }
+    }
+}
---
+++ HW9/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace HW9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewFileWorker newWorker = new NewFileWorker();
+            newWorker.MaxStorage = 128;
+            newWorker.Write();
+            newWorker.Read();
+            newWorker.Edit();
+            newWorker.Delete();
+        }
+    }
+    public abstract class FileWorker
+    {
+        public int MaxStorage { get; set; }
+
+        public abstract void Write();
+        public abstract void Read();
+        public abstract void Edit();
+        public abstract void Delete();
+    }
+    public class NewFileWorker : FileWorker
+    {
+        public override void Delete()
+        {
+            Console.WriteLine($"I can delete from txt file with max storage {MaxStorage}");
+        }
+
+        public override void Edit()
+        {
+            Console.WriteLine($"I can edit from txt file with max storage {MaxStorage}");
+        }
+
+        public override void Read()
+        {
+            Console.WriteLine($"I can read from txt file with max storage {MaxStorage}");
+        }
+
+        public override void Write()
+        {
+            Console.WriteLine($"I can write from txt file with max storage {MaxStorage}");
+        }
+    }
+}
